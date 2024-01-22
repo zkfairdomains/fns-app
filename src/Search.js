df79eaadfd74838246459d4a5191936abbd04458
ozkanayhankara85 <72730545+ozkanayhankara85@users.noreply.github.com>
2024-01-22 16:16:05 +0300

    search iconuda ekledim.

--- src/Search.js
+++ src/Search.js
@@ -1,8 +1,9 @@
+import searchIcon from './assets/images/search-icon.svg';
 function Search() {
     return ( 
         <div className="search-content">
             <form action="">
-                <img src="/assets/images/search-icon.svg" alt="" /><input type="text" placeholder="Search your domain name" />
+                <img src={searchIcon} alt="" /><input type="text" placeholder="Search your domain name" />
                 <select id="domain-choise" name="">
                     <option value="zkfair">.zkf</option>
                     <option value="other">.other</option>
