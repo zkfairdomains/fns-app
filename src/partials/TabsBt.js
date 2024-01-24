e805e41f09c8142fbbf3e81776f6b9cb6e39eb24
ozkanayhankara85 <72730545+ozkanayhankara85@users.noreply.github.com>
2024-01-25 00:43:57 +0300

    tabbootstrap.

--- src/partials/TabsBt.js
+++ src/partials/TabsBt.js
@@ -3,20 +3,29 @@ import Tabs from 'react-bootstrap/Tabs';
 
 function TabsBt() {
   return (
-    <Tabs
+    <div className='tab-outer '>
+        <Tabs
       defaultActiveKey="registrant"
       transition={false}
       id="noanim-tab-example"
-      className="mt-5"
     >
       <Tab eventKey="registrant" title="Registrant">
-        Tab content for Home
+          <h2 className='mb-3'>My Domains</h2>
+          <ul className='itemlist'>
+            <li className="copy-container d-flex">
+                <span className="copy-text w-100">Domain 1</span>
+                <div className='w-75 d-flex justify-content-end'>
+                  <span>Status: <em className='color-red'>Passive</em></span><span className='me-3 ms-3'>4$/year</span><button className="green">Click for active</button>
+                </div>
+                </li>
+            </ul>
       </Tab>
       <Tab eventKey="controller" title="Controller">
         Tab content for Profile
       </Tab>
-      
     </Tabs>
+    </div>
+    
   );
 }
 
