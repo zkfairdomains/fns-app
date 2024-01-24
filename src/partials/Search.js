7ad9335b3f4bca19f03ffe48ead878f855427d56
ozkanayhankara85 <72730545+ozkanayhankara85@users.noreply.github.com>
2024-01-24 18:04:43 +0300

    düzenlemeler devam edecek bu Brachte Pr yapınca haber et.

--- src/partials/Search.js
+++ src/partials/Search.js
@@ -1,4 +1,5 @@
 import searchIcon from '../assets/images/search-icon.svg';
+import loadericon from '../assets/images/loader-icon.svg';
 import zkfRegisterControllerABI from '../abi/ZKFRegisterController.json'
 
 import { useReadContract } from 'wagmi'
@@ -33,21 +34,26 @@ function Search() {
             </div>
         )
     }
-         
+         console.log(isPending)
     return ( 
         <div className="search-content">
             { isPending ? "Loading": ""}
             <form onSubmit={(e)=> { e.preventDefault(); return false; }}>
                 <img src={searchIcon} alt="" /><input type="text" ref={inputRef} placeholder="Search your domain name" />
-                <select id="domain-choise" disabled name="">
-                    <option value="zkfair">.zkf</option>
-                </select>
-                <button onClick={(e)=> setName(inputRef.current.value) }>SEARCH</button>
+                <span className='chainText'>.zkf</span>
+                <button onClick={(e)=> setName(inputRef.current.value) }>{isPending ? <><img src={loadericon} /></> : "SEARCH" }</button>
             </form>
             {name != "" ? <> 
             <div className="search-result-content">
                 <ul>
-                    <li className="copy-container"><span className="copy-text">{name}.zkf</span><button className={available ? "green": "red"}>{ available ? "Available": "Not Available"}</button></li>:  
+                    <li className="copy-container">
+                        <span className="copy-text w-100">{name}.zkf </span>
+                        <div className='w-50 d-flex justify-content-end'>
+                            
+                            <span className='me-3'>{available ? "4$/year": ""}</span>
+                            <button disabled={ available ? '':  'disabled' }  className={available ? "green": "red"}>{ available ? "Available to Register": "Not Available"}</button>
+                        </div>
+                        </li>
                 </ul>
             </div></>
             : <></>
