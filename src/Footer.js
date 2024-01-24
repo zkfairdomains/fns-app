500d0781dd8d93fe40c816c617941f98c6265df7
ozkanayhankara85 <72730545+ozkanayhankara85@users.noreply.github.com>
2024-01-24 11:56:16 +0300

    footer links..

--- src/Footer.js
+++ src/Footer.js
@@ -1,3 +1,7 @@
+import twittericon from './assets/images/twitter.svg' ;
+import githubicon from './assets/images/githublogo.svg' ;
+import discordicon from './assets/images/discordicon.svg' ;
+
 function Footer() {
     return ( 
         <footer className="w-100">
@@ -10,8 +14,10 @@ function Footer() {
                 </ul>
             </div>
             <div className="socialMedia">
-                <ul>
-                    <li><a href=""><img src="/assets/images/twitter.svg" alt="" /></a></li>
+                <ul className='d-flex'>
+                    <li><a href="https://twitter.com/zkfdomains " target="_blank"><img src={twittericon} alt="Tiwtter x" title="Tiwtter x" /></a></li>
+                    <li className='ms-3 me-3'><a href="https://github.com/zkfdomains" target="_blank"><img src={githubicon} alt="Github" title="Github" /></a></li>
+                    <li><a href=""><img src={discordicon} alt="Discord" title="Discord" /></a></li>
                 </ul>
             </div>
             </div>
