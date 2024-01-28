75e06a64ef865dd1780537e794ff7ab22fff0f1a
ozkanayhankara85 <72730545+ozkanayhankara85@users.noreply.github.com>
2024-01-29 01:17:22 +0300

    footer conflict fix.

--- src/partials/Footer.js
+++ src/partials/Footer.js
@@ -14,24 +14,38 @@ function Footer() {
     return ( 
         <footer className="w-100">
             <div className="container-fluid">
-            <div className="footNav">
-                <ul className="d-flex">
-                    <li><Link to="/terms">Terms</Link></li>
-                    <li><Link to="/privacy">Privacy</Link></li>
-                </ul>
-            </div>
-            <div className='footerRight'>
-            <button className="wallet-connect" onClick={() => switchChain({ chainId: Number(process.env.REACT_APP_SUPPORTED_CHAIN_ID) }) }>Add to Metamask <img src={MetaMaskLogo} /></button>
-            
-            <div className="socialMedia">
-            <ul className='d-flex'>
-                <li><a href="https://twitter.com/zkfairdomains" target="_blank"><img src={twittericon} alt="Tiwtter x" title="Tiwtter x" /></a></li>
-                <li className='ms-3 me-3'><a href="https://github.com/zkfairdomains" target="_blank"><img src={githubicon} alt="Github" title="Github" /></a></li>
-                <li><a target="_blank" href="https://discord.com/invite/QzFsgTpQ"><img src={discordicon} alt="Discord" title="Discord" /></a></li>
-                <li className='ms-3'><a target='_blank' href="https://goerli.etherscan.io/address/0xC805e17Dc9D3845374ed5E126180138CA250a581" className='imgwhiteredbg'><img className='imgwhite' src={etherscann} alt="Discord" title="Discord" /></a></li>
-            </ul>
-            </div>
-            </div>
+                <div className="footNav">
+                    <ul className="d-flex">
+                        <li><Link to="/terms">Terms</Link></li>
+                        <li><Link to="/privacy">Privacy</Link></li>
+                    </ul>
+                </div>
+                <div className='footerRight'>
+                    <button className="wallet-connect" onClick={() => switchChain({ chainId: Number(process.env.REACT_APP_SUPPORTED_CHAIN_ID) }) }>Add to Metamask <img src={MetaMaskLogo} alt="loading..." /></button>
+                    <div className="socialMedia">
+                        <ul className='d-flex'>
+                            <li>
+                                <a href={process.env.REACT_APP_TWITTER_URL} target="_blank" rel="noreferrer">
+                                    <img src={twittericon} alt="Tiwtter x" />
+                                </a>
+                            </li>
+                            <li className='ms-3 me-3'>
+                                <a href={process.env.REACT_APP_GITHUB_URL} target="_blank" rel="noreferrer">
+                                    <img src={githubicon} alt="Github"   /></a>
+                                </li>
+                            <li>
+                                <a href={process.env.REACT_APP_DISCORD_URL} target="_blank" rel="noreferrer">
+                                    <img src={discordicon} alt="Discord" />
+                                </a>
+                            </li>
+                            <li className='ms-3'>
+                                <a href={process.env.REACT_APP_CONTRACT_URL} target="_blank" rel="noreferrer" className='imgwhiteredbg'>
+                                    <img className='imgwhite' src={etherscann} alt="Contracts" />
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </div>
         </footer>
      );
